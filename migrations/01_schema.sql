9fc24d9a161bd831ce4e8a680b9fc5e1b4212bd1
dganai <dinaganai@gmail.com>
2022-02-10 14:00:11 +0000

    name column in twice, removed one

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -6,7 +6,6 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
